a75559900e94e3bc479624ae4d6e046a63bc4257
Pierrick Koch <pierrick.koch@gmail.com>
2015-03-02 14:04:42 +0100

    [tools] cleanup module start and wait

--- tools/hdl.sh
+++ tools/hdl.sh
@@ -2,20 +2,20 @@
 # rm /tmp/velodyne
 # ln -s /net/cetus/data1/robots/mana/2013-09-04-caylus/hdl-64-2/velodyne /tmp/
 
-VELODYNE_HDL_NN=64
-[ -z "$1" ] || VELODYNE_HDL_NN=$1
+velodyne=64
+[ -z "$1" ] || velodyne=$1
 
 killmymodules
 pkill tclserv
 h2 end; h2 init 20123123
 
-atlaas > atlaas.log 2>&1 &
-velodyne > velodyne.log 2>&1 &
 LANG=C tclserv -c > tclserv.log 2>&1 &
-
-# wait for module to start
-{ tail -f atlaas.log & } | sed -n '/all tasks are spawned/q'
-{ tail -f velodyne.log & } | sed -n '/all tasks are spawned/q'
+for mod in atlaas velodyne; do
+    echo > $mod.log && $mod > $mod.log 2>&1 &
+    { tail -f $mod.log & } | sed -n '/all tasks are spawned/q' & pid="$pid $!"
+done
+# wait for modules to start
+wait $pid
 
 eltclsh << EOF
 package require genom
@@ -24,7 +24,7 @@ lm atlaas
 lm velodyne
 
 # mode model input_pcap enable_organization use_pom pos_poster
-velodyne::Init VELODYNE_READ VELODYNE_HDL${VELODYNE_HDL_NN} NULL VELODYNE_TRUE VELODYNE_FALSE NULL
+velodyne::Init VELODYNE_READ VELODYNE_HDL${velodyne} NULL VELODYNE_TRUE VELODYNE_FALSE NULL
 
 # base_name start_number
 velodyne::SetReadModeParams /tmp/velodyne/shot 0
@@ -34,6 +34,7 @@ atlaas::Init 120 120 0.1 398369.61 4903128.61 0.0 31 1 velodyneThreeDImage NULL
 
 while { ! [catch {velodyne::OneShot -180 180}] } {
     atlaas::Fuse
+    puts -nonewline "."
 }
 
 atlaas::MakeRegion
